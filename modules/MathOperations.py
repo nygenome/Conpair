efab4fafd317529738221d809de1a6462977ebaf
Ewa A. Grabowska <egrabowska@nygenome.org>
2016-03-29 16:38:40 -0400

    Update: updated year of the copyrights registration

--- modules/MathOperations.py
+++ modules/MathOperations.py
@@ -8,7 +8,7 @@
 
 # New York Genome Center
 # SOFTWARE COPYRIGHT NOTICE AGREEMENT
-# This software and its documentation are copyright (2014) by the New York
+# This software and its documentation are copyright (2016) by the New York
 # Genome Center. All rights are reserved. This software is supplied without
 # any warranty or guaranteed support whatsoever. The New York Genome Center
 # cannot be responsible for its use, misuse, or functionality.
